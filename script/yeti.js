61b25eea2d28267d2b324dd4b4e510338eca36de
Randy Merrill <Zoramite@gmail.com>
2011-12-01 21:59:17 -0800

    Added the `localStorage` for the loan and payment data.
    Closes #1

--- script/yeti.js
+++ script/yeti.js
@@ -10,6 +10,7 @@
 	var containers = {};
 	var buttons = {};
 	var slideDuration = 380;
+	var allowLocalSave = true;
 	
 	$(function(){
 		containers.loan = $('#loans');
@@ -30,14 +31,28 @@
 			addLoan();
 		});
 		
-		// Add initial loan
-		addLoan(5000, 8.5, 50);
+		containers.loan.on('change', saveData);
+		containers.payment.on('change', saveData);
+		
+		// Check for previously saved loans
+		if(localStorage.loans) {
+			var loans = JSON.parse(localStorage.loans);
+			
+			$.each(loans, function(i, value) {
+				addLoan(value.principal, value.rate, value.minPayment);
+			});
+		} else {
+			// Add sample loan
+			addLoan(5000, 8.5, 50);
+		}
+		
+		$('input[name="payment"]', containers.payment).val(localStorage.payment || 0.00);
 	});
 	
-	function addLoan(principal, interest, minPayment) {
+	function addLoan(principal, rate, minPayment) {
 		var loan = $.tmpl('loan', {
 			principal: principal || 0,
-			interest: interest || .0,
+			rate: rate || 0.0,
 			minPayment: minPayment || 0
 		});
 		
@@ -53,9 +68,37 @@
 		loan.slideUp(slideDuration, function(){
 			loan.remove();
 			
+			saveData();
+			
 			if(!$('.loan', containers.loan).length) {
 				addLoan();
 			}
 		});
+	}
+	
+	function saveData() {
+		if(!allowLocalSave) {
+			return;
+		}
+		
+		// Save the loans
+		var loans = [];
+		
+		$('.loan', containers.loan).each(function(i, element){
+			var loan = {};
+			
+			loan.principal = parseFloat($('input[name="principal"]', element).val()) || 0;
+			loan.rate = parseFloat($('input[name="rate"]', element).val()) || 0.0;
+			loan.minPayment = parseFloat($('input[name="minPayment"]', element).val()) || 0;
+			
+			if(loan.principal > 0) {
+				loans.push(loan);
+			}
+		});
+		
+		localStorage.loans = JSON.stringify(loans);
+		
+		// Save the payment
+		localStorage.payment = parseFloat($('input[name="payment"]', containers.payment).val()) || 0.0;
 	}
 }));
