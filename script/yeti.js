004b93e879387bec3b2d1a935afa0d52caff89d6
Randy Merrill <Zoramite@gmail.com>
2011-12-02 12:22:56 -0800

    Adding an error message when the data for the loan or payment is invalid.
    Also added basic status changes to the status button.
    
    Closes #5

--- script/yeti.js
+++ script/yeti.js
@@ -11,6 +11,8 @@
 	var buttons = {};
 	var slideDuration = 380;
 	var allowLocalSave = true;
+	var currency = '$';
+	var ppy = 12;
 	
 	$(function(){
 		containers.loan = $('#loans');
@@ -18,21 +20,39 @@
 		containers.details = $('#details');
 		containers.extraDetails = $('#extraDetails');
 		
-		$('#loanTemplate').template('loan');
 		$('#buttonTemplate').template('button');
+		$('#errorTemplate').template('error');
+		$('#loanTemplate').template('loan');
+		$('#paymentTemplate').template('payment');
 		
-		buttons.addLoan = $.tmpl('button', {
+		// Add an add button for loans
+		containers.addLoan = $.tmpl('button', {
 			value: '+ Add'
 		});
 		
-		buttons.addLoan.insertAfter(containers.loan);
+		containers.addLoan.insertAfter(containers.loan);
 		
-		$('input', buttons.addLoan).click(function() {
+		buttons.addLoan = $('input', containers.addLoan).click(function() {
 			addLoan();
 		});
 		
-		containers.loan.on('change', saveData);
-		containers.payment.on('change', saveData);
+		// Add a calculation button
+		containers.calculate = $.tmpl('button', {
+			className: 'status',
+			value: 'Waiting For Snow&hellip;'
+		});
+		
+		containers.calculate.insertAfter(containers.payment);
+		
+		buttons.calculate = $('input', containers.calculate);
+		
+		buttons.calculate.click(function() {
+			console.log('Calculate clicked.');
+		});
+		
+		// Bind to the changes to save the form data
+		containers.loan.on('input', saveData);
+		containers.payment.on('input', saveData);
 		
 		// Check for previously saved loans
 		if(localStorage.loans) {
@@ -46,14 +66,22 @@
 			addLoan(5000, 8.5, 50);
 		}
 		
-		$('input[name="payment"]', containers.payment).val(localStorage.payment || 0.00);
+		// Add the payment information
+		$.tmpl('payment', {
+			currency: currency,
+			payment: localStorage.payment || 0.00
+		}).appendTo(containers.payment);
+		
+		// Trigger the check for valid data
+		checkStatus();
 	});
 	
 	function addLoan(principal, rate, minPayment) {
 		var loan = $.tmpl('loan', {
 			principal: principal || 0,
 			rate: rate || 0.0,
-			minPayment: minPayment || 0
+			minPayment: minPayment || 0,
+			currency: currency
 		});
 		
 		loan.hide().appendTo(containers.loan).slideDown(slideDuration, function() {
@@ -63,6 +91,98 @@
 			
 			// Auto focus on the new loan
 			$('input[type="number"]:first', loan).focus();
+		});
+	}
+	
+	function checkStatus() {
+		// Start off with the button disabled
+		buttons.calculate.attr('disabled', 'disabled');
+		buttons.calculate.va;('Checking for Snow…');
+		
+		// Check for valid loan data
+		var isValid = true;
+		var totalPeriodInterest = 0.0;
+		var loans = [];
+		
+		$('.loan', containers.loan).each(function(i, element){
+			var loan = {};
+			var ele = $(element);
+			
+			loan.principal = parseFloat($('input[name="principal"]', element).val()) || 0;
+			loan.rate = parseFloat($('input[name="rate"]', element).val()) || 0.0;
+			loan.minPayment = parseFloat($('input[name="minPayment"]', element).val()) || 0;
+			
+			// Only care about loans with principals
+			if(loan.principal <= 0) {
+				return;
+			}
+			
+			// Need a minimum payment if there is an interest rate
+			if(loan.rate > 0 && loan.minPayment == 0) {
+				setError(ele, 'Needs a minimum payment since there is an interest rate');
+				isValid = false;
+				
+				return;
+			}
+			
+			var periodRate = (loan.rate / ppy);
+			var periodInterest = parseFloat((loan.principal * (periodRate / 100)).toFixed(2));
+			
+			// Need a minimum payment that pays at least the interest
+			if(loan.rate > 0 && loan.minPayment < periodInterest) {
+				setError(ele, 'Needs a minimum payment greater than the interest charge: ' + currency + periodInterest);
+				isValid = false;
+				
+				return;
+			}
+			
+			// Remove any lingering errors
+			if(ele.hasClass('error')) {
+				removeError(ele);
+			}
+			
+			// Keep a running sum of the interest amount
+			totalPeriodInterest += periodInterest;
+			
+			loans.push(loan);
+		});
+		
+		// Check for valid payment data
+		var payment = $('input[name="payment"]', containers.payment).val();
+		var paymentContainer = $('.payment', containers.payment);
+		
+		if(payment < totalPeriodInterest) {
+			setError(paymentContainer, 'Needs a repayment amount greater than the interest charges: ' + currency + totalPeriodInterest);
+			isValid = false;
+		} else {
+			removeError(paymentContainer);
+		}
+		
+		// Only continue if all loans and payment values are valid
+		if(!loans.length || !isValid) {
+			buttons.calculate.val('Waiting for Snow…');
+			
+			return;
+		}
+		
+		buttons.calculate.val('Pack Snowballs!');
+		buttons.calculate.removeAttr('disabled');
+		
+		// Check if the schedule has already been calculated
+	}
+	
+	function removeError(element) {
+		if(!element.hasClass('error')) {
+			return;
+		};
+		
+		var error = element.data('error');
+		
+		error.slideUp(slideDuration, function(){
+			element.removeClass('error');
+			element.removeData('error');
+			
+			error.remove();
 		});
 	}
 	
@@ -103,5 +223,26 @@
 		
 		// Save the payment
 		localStorage.payment = parseFloat($('input[name="payment"]', containers.payment).val()) || 0.0;
+		
+		// Recheck the loan data
+		checkStatus();
+	}
+	
+	function setError(element, message) {
+		var error;
+		
+		error = element.data('error');
+		
+		if(!error) {
+			error = $.tmpl('error', {
+				className: 'message'
+			});
+			
+			element.data('error', error);
+			element.append(error);
+		}
+		
+		error.text('⇒ ' + message);
+		element.addClass('error');
 	}
 }));
