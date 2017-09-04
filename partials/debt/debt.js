db5fd336658e842e60816b43c542f01f306082b7
Randy Merrill <Zoramite@gmail.com>
2017-09-04 08:37:36 -0600

    Stubbing in the debt display.

--- partials/debt/debt.js
+++ partials/debt/debt.js
@@ -1,7 +1,23 @@
 export default class DebtPartial {
   constructor(config) {
     this.config = config || {}
+    this.elContainer = document.querySelector('.debt')
+    this.elDebtAdd = this.elContainer.querySelector('.debt__add')
+    this.elDebtList = this.elContainer.querySelector('.debt__list')
+    this.elTemplateDebt = this.elContainer.querySelector('#debt-template')
 
-    console.log('Debt welcomes you');
+    this.elDebtAdd.addEventListener('click', this.addDebt.bind(this))
+
+    // Create initial debt information.
+    this.addDebt()
+  }
+
+  addDebt(e) {
+    if (e) {
+      e.preventDefault()
+    }
+
+    const currentDebt = document.importNode(this.elTemplateDebt.content, true)
+    this.elDebtList.appendChild(currentDebt)
   }
 }
